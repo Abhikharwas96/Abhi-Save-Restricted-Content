951e6bcc83ecf5a1ac5d4f26838153a9f1387a0f
Abhikharwas96 <kharwasabhishek@gmail.com>
2026-07-10 13:35:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,20 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+
+
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-	TechVJUser.start()
+	AbhiUser = Client("AbhiKumar", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
+	AbhiUser.start()
 else:
-    TechVJUser = None
+    AbhiUser = None
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "techvj login",
+            "Abhi login",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -28,7 +27,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Bots')
+        print('Bot Started Powered By @Abhi_Bots')
 
     async def stop(self, *args):
 
@@ -39,6 +38,3 @@ if __name__ == "__main__":
     bot = Bot()
     bot.run()
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01

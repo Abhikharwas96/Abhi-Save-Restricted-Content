0690773fa9dcef747ea6c1b0dd45d1f3df7daf47
Abhikharwas96 <kharwasabhishek@gmail.com>
2026-07-10 14:01:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ else:
     STRING_SESSION =1BVtsOHYBuwx-yNsL_WjHHf6U5MVokGunIDcx0O-1mfR8lBRfCBA4vXvwIz73Aqn2pQjKogkgCnXvmFBRpFAWJgyo_RRZ3mqcFzNJM3vheaP4sE3p4Se0d2hyliGt-JZWqetv9OkXakdOOAsIvqrDwJh--YXXMLJgQKTGhKLcp57Xui01yIdGE-nF6LtySB9cP-CYJL7kz7Sd1_8iRVriL5zanRJhQzGkKKJG2ibpT5s4sdV2uoyuHoNmaRnt12wKPzWPafqGubTeqi12qyNgIh_5GJjbYSM9Uzq-_kYoErBxO_3ZtAQd_BdgjNhDUCuGNi2rXJQtJTuiL6FRT4YaJkOyzDGJJaE=
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8988323460:AAFW5ziwhBgl_xFA04PMpJZb7tC0-gurtpg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","8983611509:AAGURaQ5d3UOV4ifDm_y9HP7M0i9MXF7xF4")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "33016948"))
@@ -21,7 +21,7 @@ API_ID = int(os.environ.get("API_ID", "33016948"))
 API_HASH = os.environ.get("API_HASH", "0d86424aba3761436da03c5331c38509")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "8509692514"))
+ADMINS = int(os.environ.get("ADMINS", "6863647592"))
 
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
